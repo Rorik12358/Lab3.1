14dba839965ffe8307ea612a261ba242671ee717
Rorik <mazurov-roman@mail.ru>
2016-04-06 21:16:03 +0300

    not completed 2

--- src/lab3_1_2/CatBufferedReader.java
+++ src/lab3_1_2/CatBufferedReader.java
@@ -1,20 +1,26 @@
 package lab3_1_2;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by m18 on 04.04.2016.
  */
 public class CatBufferedReader {
-//    public static void main(String[] args) {
-//        try {
-//            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-//            String line;
-//            while ((line = reader.readLine()!= false)) {
-//                System.out.println(reader.readLine());
-//            }
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    public static void main(String[] args) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            while (true) {
+                String line = reader.readLine();
+                if (line == null) break;
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
